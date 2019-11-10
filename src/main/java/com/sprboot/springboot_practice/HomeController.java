9be49ad7c1d38f0cc40dd4ddf924a9adc924d01d
illyashk50@gmail.com <illyashk50@gmail.com>
2019-11-10 16:39:52 +0200

    Initial Commit: Spring Boot

---
+++ src/main/java/com/sprboot/springboot_practice/HomeController.java
@@ -0,0 +1,19 @@
+package com.sprboot.springboot_practice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("home")
+    public ModelAndView home(@RequestParam("name") String myName) {
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", myName);
+
+        return mv;
+    }
+}
